d776f7cb8885a29ffef4ae599aaa329ee2af8025
Radek Srb <radeksrb@email.cz>
2022-10-12 11:10:05 +0200

    Extension of 01_mpi4py.

--- testing/integrated/01_mpi4py/script.py
+++ testing/integrated/01_mpi4py/script.py
@@ -1,37 +1,37 @@
 from mpi4py import MPI
+import hashlib
+import random
+import numpy as np
 
 comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
+size = comm.Get_size()
 
-if rank == 0:
-    data = {'a': 1, 'b': 3.14}
-    print("P0 sending to P1: {}", data)
-    req = comm.isend(data, dest=1, tag=11)
-    req.wait()
-    
-    data = {'c': 2, 'd': 6.28}
-    print("P0 sending to P2: {}", data)
-    req = comm.isend(data, dest=2, tag=12)
-    req.wait()
-    print("P0 finished")
-elif rank == 1:
-    req = comm.irecv(source=0, tag=11)
-    data = req.wait()
-    print("P1 received: {}", data)
-    
-    data["a"] = 3
-    data["b"] = 3*3.14
-    print("P1 sending to P3: {}", data)
-    req = comm.isend(data, dest=3, tag=13)
-    req.wait()
-    print("P1 finished")
-elif rank == 2:
-    req = comm.irecv(source=0, tag=12)
-    data = req.wait()
-    print("P2 received: {}", data)
-    print("P2 finished")
-elif rank == 3:
-    req = comm.irecv(source=1, tag=13)
-    data = req.wait()
-    print("P3 received: {}", data)
-    print("P3 finished")
+seed = hashlib.sha256(str(rank).encode()).digest()
+random.seed(seed)
+
+rand_max = 1000_000
+num_sends = 100_000
+num_iter = 10
+
+for i in range(num_iter):
+    sum = 0
+    for j in range(1, num_sends + 1):
+        data_out = random.randrange(rand_max)
+        dest = (rank + j) % size
+        req = comm.isend(data_out, dest=dest)
+        req.wait()
+
+        req = comm.irecv()
+        data_in = req.wait()
+        sum += data_out
+        sum += data_in
+        sum %= 275_604_541
+
+    recvbuf = np.empty(1, dtype=int)
+    sendbuf = sum * np.ones(size, dtype=int)
+    comm.Reduce_scatter(sendbuf, recvbuf)
+
+    print("[{}] {}".format(rank, recvbuf[0]))
+
+print("P{} finished".format(rank))
